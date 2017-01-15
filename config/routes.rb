bd0434d1bcf75255b3335f2addda6422bc6f16c6
brendanoneill2016 <brendan@customking.ie>
2017-01-15 16:19:59 +0000

    added articles

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ end
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contactus' => 'pages#contactus'
+  get 'news' => 'pages#news'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
